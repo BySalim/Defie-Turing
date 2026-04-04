bffa7405b71c6bbc5bb4889877663009a30dc1a7
Delta Salim <salimouedz@gmail.com>
2026-04-04 06:09:59 +0000

    Correction de l'orthographe de la fonction 'mirior' en 'miroir' dans le fichier problem11.py

--- problem11.py
+++ problem11.py
@@ -10,7 +10,7 @@ from modules.timer import mesure_temps
 # Idée :
 # On parcours de 10 millions en décrémentant et on vérifie que la propriété est respecté
 
-def mirior(nombre):
+def miroir(nombre):
     """Retourne le miroir d'un nombre"""
     return int(str(nombre)[::-1])
 
@@ -18,7 +18,7 @@ def mirior(nombre):
 def plus_grand_nombre(limite):
     """Retourne le plus grand nombre inférieur à [limite] respectant la propriété miroir(n) = 4 x n"""
     for n in range(limite, 0, -1):
-        if n * 4 == mirior(n):
+        if n * 4 == miroir(n):
             return n
 
 if __name__ == "__main__":
